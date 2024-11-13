5d3e9dc4a395d7bfddab63b18a24539af0eaac4d
iraj oshadha <oshadairaj33@gmail.com>
2024-11-13 22:53:38 +0530

    initial commit

---
+++ Server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const TodoModel = require("./Models/Todo");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
+
+app.get("/get", (req, res) => {
+  TodoModel.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndUpdate({ _id: id }, { done: true })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
